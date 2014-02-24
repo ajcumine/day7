3cd5716851abcfd15d2552f59b8af0858907d9a5
Andrew <aj.cumine@btinternet.com>
2014-02-24 14:49:36 +0000

    refactoring challenge.rb and add .gitignore for save file

--- challenge.rb
+++ challenge.rb
@@ -1,28 +1,27 @@
-
-
-loop do
+def interactive_menu
   puts "Please select an option from the menu:"
   puts 'Type "input" to store text for later use'
   puts 'Type "save" to save stored text to hello_from_ruby.txt'
   puts 'Type "load" to load saved data from hello_from_ruby.txt'
   puts 'Type "introspect" to view the stored code'
   puts 'Type "exit" to leave the program'
+end
+
+loop do
+  interactive_menu
   select = gets.chomp
   case select
   when "input"
     puts "Please provide input text to be stored: "
     @text = gets.chomp
-    puts "#{@text}"
   when "save"
-    File.open("hello_from_ruby.txt", "w") {|f|f.puts "#{@text}"}
+    File.open("hello_from_ruby.txt", "w") {|f| f.puts "#{@text}"}
     puts "Text saved to file"
   when "load"
-    file = File.open("hello_from_ruby.txt", "r")
-    @text = file.read
+    File.open("hello_from_ruby.txt", "r") {|f| @text = f.read}
     puts "Text loaded from file"
   when "introspect"
-    file = File.open("hello_from_ruby.txt", "r")
-    puts file.read
+    File.open("hello_from_ruby.txt", "r") {|f| puts f.read}
   when "exit"
     exit
   else
